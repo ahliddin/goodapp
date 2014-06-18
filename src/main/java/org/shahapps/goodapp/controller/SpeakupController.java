8012fa8c5d9fb5be145443dcf59cf6eeede683b0
unknown <ahliddin.gaffar@gmail.com>
2014-06-18 18:43:20 +0200

    Some manipulations/bug fixing around cross-domain requests issue

--- src/main/java/org/shahapps/goodapp/controller/SpeakupController.java
+++ src/main/java/org/shahapps/goodapp/controller/SpeakupController.java
@@ -1,15 +1,22 @@
 package org.shahapps.goodapp.controller;
 
 
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.shahapps.goodapp.dao.FeedbackDao;
 import org.shahapps.goodapp.domain.Feedback;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import org.springframework.validation.BindingResult;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 
@@ -23,40 +30,45 @@ public class SpeakupController {
 		return "index";
 	}
 	
-    @RequestMapping(value="/feedbacks", method=RequestMethod.POST) //, consumes="application/json"
-    public Integer addFeedback (@RequestBody @Valid 
-    										Feedback newFeedBack,
-    										BindingResult bindingResult) {
+    @RequestMapping(value="/feedbacks3", method=RequestMethod.POST, consumes = {"application/json"}) 
+    public Integer addFeedback3 (@RequestBody @Valid  Feedback newFeedback,
+    								BindingResult bindingResult ) {
     	
     	if (bindingResult.hasErrors()) {
     		System.out.println ("\nWrong input!!!\n");
     		return 1;
     	}
-    	feedbackDao.register(newFeedBack);
+    	feedbackDao.register(newFeedback);
     	return 0;
     }
-    
-    @RequestMapping(value="/feedbacks2", method=RequestMethod.POST) //, consumes="application/json"
-    public int addFeedback2 (@RequestBody @Valid 
-    										Feedback newFeedBack,
-    										BindingResult bindingResult) {
+
+    @RequestMapping(value="/feedbacks2", method=RequestMethod.POST, consumes={"application/json"}) 
+    public String addFeedback2 (@RequestBody @Valid Feedback newFeedback,
+    							BindingResult bindingResult ) {
     	
     	if (bindingResult.hasErrors()) {
     		System.out.println ("\nWrong input!!!\n");
-    		return 1;
+    		return "bad";
     	}
-    	feedbackDao.register(newFeedBack);
-    	return 0;
+    	feedbackDao.register(newFeedback);
+    	return "good";
     }
     
-    @RequestMapping(value="/feedbacks3", method=RequestMethod.POST) //, consumes="application/json"
-    public void addFeedback3 (@RequestBody @Valid 
-    										Feedback newFeedBack,
-    										BindingResult bindingResult) {
+    @RequestMapping(value="/feedbacks", method=RequestMethod.POST, consumes = {"application/json"}) 
+    public ResponseEntity<Feedback> addFeedback (@RequestBody @Valid  Feedback newFeedback,
+    								BindingResult bindingResult ) {
     	
-    	if (bindingResult.hasErrors()) {
-    		System.out.println ("\nWrong input!!!\n");
-    	}
-    	feedbackDao.register(newFeedBack);
+    	HttpHeaders headers = addAccessControllAllowOrigin();
+    	ResponseEntity<Feedback> entity = new ResponseEntity<Feedback>(headers, HttpStatus.OK);
+    	feedbackDao.register(newFeedback);
+    	return entity;
+    	
     }
+    
+    private HttpHeaders addAccessControllAllowOrigin() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        return headers;
+    }
+    
 }	
--- src/main/java/org/shahapps/goodapp/filter/CorsFilter.java
+++ src/main/java/org/shahapps/goodapp/filter/CorsFilter.java
@@ -14,12 +14,13 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
                                     throws ServletException, IOException {
-       // if (request.getHeader("Access-Control-Request-Method") != null) {
+    	if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
             // CORS "pre-flight" request
-        response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-//        }
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, sid, mycustom, smuser");
+            response.addHeader("Access-Control-Max-Age", "1800");//30 min
+        }
         filterChain.doFilter(request, response);
     }
 }
